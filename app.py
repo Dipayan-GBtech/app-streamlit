6284dae1279698c0c87a07bf08e8084b2a4e77e2
Dipayan-GBtech <droy@gbtechservice.com>
2025-08-06 18:11:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,8 +73,8 @@ def generate_with_together(prompt):
                 "Content-Type": "application/json"
             },
             json={
-                #"model": "Qwen/Qwen2-72B-Instruct",
-                "model": "Qwen/Qwen2.5-VL-72B-Instruct",
+                "model": "Qwen/Qwen2-72B-Instruct",
+                #"model": "Qwen/Qwen2.5-VL-72B-Instruct",
                 #"model":"mistralai/Mixtral-8x7B-Instruct-v0.1",
                 #"model": "Qwen/Qwen2.5-7B-Instruct-Turbo",
                 "messages": [
