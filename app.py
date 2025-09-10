2f9b3853653b1d7b94d96c63662aad2be587883d
Dipayan-GBtech <droy@gbtechservice.com>
2025-09-10 19:32:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def generate_with_together(prompt):
             json={
                 #"model": "Qwen/Qwen2-72B-Instruct",
                 #"model": "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free",
-                "model": "Qwen/Qwen3-235B-A22B-Instruct-2507-tput",
+                "model": "Qwen/Qwen2.5-14B-Instruct",
                 #"model":"openai/gpt-oss-120b",
                 #"model": "Qwen/Qwen2.5-14B-Instruct",
                 "messages": [
